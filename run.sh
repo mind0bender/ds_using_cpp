4ca26c319bd7fc54beb80d048853cfe43cb2df06
mind0bender <mind0bender@gmail.com>
2024-04-03 13:29:26 +0530

    { "C": ["run.sh"] }

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# the first argument is the file number, like 11.1
+# and the filename should be constructed using this file number 
+
+
+
+
+filename="./practical$1.cpp"
+
+if [ -z $filename ]; then
+    echo "Please provide a file name"
+    exit 1
+else
+    if [ ! -f $filename ]; then
+        echo "File not found!"
+        echo "$filename"
+        exit 1
+    else 
+        g++ -std=c++11 -o "practical$1" "$filename"
+        "./practical$1"
+    fi
+fi
+
+echo "running "$1
